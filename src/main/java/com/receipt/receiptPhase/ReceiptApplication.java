6293df955c9b01416a8cf8cd7a5bd5b0dae0f70b
Tamilselvan <tamilselvan@aggrandizeventure.com>
2026-07-02 12:52:38 +0800

    database connection implemented

--- src/main/java/com/receipt/receiptPhase/ReceiptApplication.java
+++ src/main/java/com/receipt/receiptPhase/ReceiptApplication.java
@@ -2,23 +2,10 @@ package com.receipt.receiptPhase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ReceiptApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReceiptApplication.class, args);
     }
 }
-
-//package com.receipt.receiptPhase;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class ReceiptApplication {
-//    public static void main(String[] args) {
-//        SpringApplication.run(ReceiptApplication.class, args);
-//    }
-//}
